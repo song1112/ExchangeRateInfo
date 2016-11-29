17a1191497191ae90293ada004a6738314237db8
Administrator <admin@example.com>
2016-11-29 12:17:53 +0800

    init

---
+++ craw/crawbktw.py
@@ -0,0 +1,19 @@
+import requests
+from selenium import webdriver
+
+class Crawbktw:
+    
+    def __init__(self):
+        self.driver = webdriver.PhantomJS()
+
+    def buy_cash_rate(self):
+        pass
+
+    def sell_cash_rate(self):
+        pass
+
+    def buy_spot_rate(self):
+        pass
+
+    def sell_spot_rate(self):
+        pass
